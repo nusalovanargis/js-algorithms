75cf305eb8109e52789188a76da8e97c0955318b
Nargıs <nargisnusalova@Nargs-MacBook-Pro.local>
2024-02-23 11:23:11 +0400

    merged-two-lists

---
+++ script.js
@@ -0,0 +1,26 @@
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+
+var mergeTwoLists = (list1, list2) => {
+    if (!list1) {
+        return list2;
+    };
+
+    if (!list2) {
+        return list1;
+    };
+
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }
+
+    if (list1.val > list2.val) {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+}
